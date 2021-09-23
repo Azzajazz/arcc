b2cc2c741b59b351ef4b58fa0e8b3557831a9de7
Adam <u5799578@anu.edu.au>
2021-09-23 14:47:15 +1000

    Extended lexer functionality

--- arcc/include/lexer.h
+++ arcc/include/lexer.h
@@ -5,8 +5,17 @@ typedef struct LEXER_STRUCT
 {
     char* src;
     int pos;
+    int src_len;
 } lexer_t;
 
-lexer_t* lexer_create(char* src, int pos);
+lexer_t* lexer_create_with_pos(char* src, int pos);
+
+lexer_t* lexer_create(char* src);
+
+void lexer_advance_by(lexer_t* lexer, int offset);
+
+void lexer_advance(lexer_t* lexer);
+
+char lexer_peek(lexer_t* lexer, int offset);
 
 #endif
--- arcc/src/lexer.c
+++ arcc/src/lexer.c
@@ -1,9 +1,33 @@
 #include "lexer.h"
+#include "macros.h"
 
-lexer_t* lexer_create(char* src, int pos)
+#include <string.h>
+
+lexer_t* lexer_create_with_pos(char* src, int pos)
 {
     lexer_t* lexer = malloc(sizeof(struct LEXER_STRUCT));
     lexer->src = src;
     lexer->pos = pos;
+    lexer->src_len = strlen(src);
     return lexer;
+}
+
+lexer_t* lexer_create(char* src)
+{
+    return lexer_create_with_pos(src, 0);
+}
+
+void lexer_advance_by(lexer_t* lexer, int offset) {
+    lexer->pos = MIN(lexer->pos + offset, lexer->src_len - 1);
+}
+
+void lexer_advance(lexer_t* lexer)
+{
+    lexer_advance_by(lexer, 1);
+}
+
+char lexer_peek(lexer_t* lexer, int offset)
+{
+    int index = MIN(lexer->pos + offset, lexer->src_len - 1);
+    return lexer->src[index];
 }
